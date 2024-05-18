ab99c0e286113b387450c5395304fd1855af2657
Hossain K. M. <awarehossain@gmail.com>
2024-05-18 21:28:41 +0600

    cm-4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    
-  }
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+};
 
-  module.exports = nextConfig
+module.exports = nextConfig;
